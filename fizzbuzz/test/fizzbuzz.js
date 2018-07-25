2cc2d0472d5f5f46f3dbf67427774ee658b608f3
Peter Simpson <psimpson@stelladot.com>
2018-07-24 19:09:58 -0700

    Adds fizzbuzz template

---
+++ fizzbuzz/test/fizzbuzz.js
@@ -0,0 +1,4 @@
+// Entering a number divisible by 3 should return 'fizz'.
+// Entering a number divisible by 5 should return 'buzz'.
+// Entering a number divisible by 3 and 5 should return 'fizzbuzz'.
+// Otherwise, it should return the input as a string.
